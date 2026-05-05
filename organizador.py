63c1420b5e7d17944ab3dd9446fbbcd24faaf562
carlostsucamoto <caduchiarelli@hotmail.com>
2026-05-04 23:44:29 -0300

    feat: adiciona função organizar com movimentação de arquivos

--- organizador.py
+++ organizador.py
@@ -14,3 +14,13 @@ tipos = {
     "Apresentacoes" : [".ppt", ".pptx"]
 
 }
+def organizar():
+    for item in os.listdir(pasta_alvo):
+        caminho_completo = os.path.join(pasta_alvo, item)
+        if os.path.isfile(caminho_completo):
+            nome, extensao = os.path.splitext(item)
+            for pasta, extensoes in tipos.items():
+                if extensao.lower() in extensoes:
+                    destino = os.path.join(pasta_alvo, pasta)
+                    os.makedirs(destino, exist_ok=True)
+                    shutil.move(caminho_completo, destino)
